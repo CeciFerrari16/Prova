8cba3ee7c12c3234276c1b1465ce78ac4d0bef27
CeciFerrari16 <cecilia.ferrari2@liceoariostospallanzani-re.edu.it>
2020-11-15 14:48:12 +0100

    Aggiunto frasi motivazionali

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 print("Ciao, come va?")
-print("Sto testando le mie conoscenze facendo alcune prove e utilizzando comandi imparati anche l'anno scorso")
+print("Sto testando le mie conoscenze facendo alcune prove sul terminale e riutilizzando comandi "\
+"imparati anche l'anno scorso")
 print(":)")
-vita = input("Come va la vita?")
+vita = input("Come va la vita? ")
 print("Ah quindi la tua vita va", vita, "mmh")
 print("Beh ci vediamo! <3")
+print()
+print("ASPETTA!!!")
+input()
+print("Ricordati che tutte le cose sono difficili prima di diventare facili, quindi "\
+    "continua a provare e non ti arrendere")
+print("Conto su di te!")
