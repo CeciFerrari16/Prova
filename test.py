2b6847a76897c96ec5ce7051f0d7ee63d882c579
CeciFerrari16 <cecilia.ferrari2@liceoariostospallanzani-re.edu.it>
2020-11-15 15:30:36 +0100

    Aggiunto liste di aggettivi per facilitare la risposta

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
+adj1 = [ "bene", "meglio", "molto bene", "fantasticamente", "petalosamente" ]
+adj2 = [ "male", "peggio", "molto male", "male, non mi parlare", "voglio morire", "affari miei" ]
+
 print("Ciao, come va?")
 print("Sto testando le mie conoscenze facendo alcune prove sul terminale e riutilizzando comandi "\
-"imparati anche l'anno scorso")
-print(":)")
-vita = input("Come va la vita? ")
-print("Ah quindi la tua vita va", vita, "mmh")
-print("Beh ci vediamo! <3")
-print()
-print("ASPETTA!!!")
+"imparati anche l'anno scorso :) ...")
 input()
+print("Come va la vita?")
+print()
+print("Puoi scegliere tra queste opzioni per rispondere: (oppure puoi personalizzarla)")
+print("; ".join(adj1))
+print("; ".join(adj2))
+print()
+vita = input("La tua risposta: ")
+print()
+print("Ah quindi la tua vita va", vita)
+print()
+print("Beh ciao, ci vediamo! <3")
+input()
+print("SCHERZAVO ASPETTA!!!")
 print("Ricordati che tutte le cose sono difficili prima di diventare facili, quindi "\
-    "continua a provare e non ti arrendere")
+    "continua a provare e non ti arrendere.")
 print("Conto su di te!")
